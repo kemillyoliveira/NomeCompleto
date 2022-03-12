1dc5c171dd7d9517950f29955f077597228e93c6
kemilly <okemilly89@gmail.com>
2022-03-11 22:23:55 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string nome, sobrenome, sobrenomeEmMaiusculo;
+
+Console.Write("Digite seu primeiro nome: ");
+nome = Console.ReadLine()!;
+
+Console.Write("Digite seu segundo nome: ");
+sobrenome = Console.ReadLine()!;
+
+sobrenomeEmMaiusculo = sobrenome.ToUpper();
+
+Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+Console.WriteLine($"Nome completo: {sobrenomeEmMaiusculo}, {nome}");
+
